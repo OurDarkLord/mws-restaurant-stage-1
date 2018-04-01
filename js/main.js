d7ad876e063202c4fbf32e40aa9a641d1f0f95c8
unknown <tim.asscherickx@hotmail.com>
2018-04-01 21:53:44 +0200

    Inserting API key Styling of header and filter options

--- js/main.js
+++ js/main.js
@@ -75,6 +75,7 @@ window.initMap = () => {
     lat: 40.722216,
     lng: -73.987501
   };
+  console.log('test init map00');
   self.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: loc,
